f1a5d3fc2fa54a8a33330022fba3e6926cea9b2f
Javier  Amor De La Cruz <javieramor@MacBook-Pro-de-Javier.local>
2016-09-14 17:59:38 +0200

    Hola Mundo v1.0

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var Slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        msgLabel.alpha = CGFloat(sender.value)
+    }
+    
+    @IBAction func updateHola(_ sender: UIButton) {
+        msgLabel.text = "Hola"
+        Slider.value=0.5
+        mapView.mapType=MKMapType.hybrid
+        sliderMoved(Slider)
+        
+    }
+    
+    @IBAction func updateMundo(_ sender: UIButton) {
+        msgLabel.text = "Mundo"
+        
+        let center = CLLocationCoordinate2D(latitude: 40.452445, longitude: -3.726162)
+        let span = MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+        let reg = MKCoordinateRegion(center: center, span: span)
+
+        mapView.setRegion(reg, animated: true)
+        mapView.mapType=MKMapType.satellite
+    }
+    
+    @IBAction func mostrarPiramides(_ sender: UIButton) {
+        msgLabel.text = "Pirámides"
+        
+        let center = CLLocationCoordinate2D(latitude: 29.9773008, longitude: 31.1303068)
+        let span = MKCoordinateSpan(latitudeDelta: 0.012, longitudeDelta: 0.012)
+        let reg = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(reg, animated: true)
+        mapView.mapType=MKMapType.satellite
+    }
+
+    
 
 }
 
